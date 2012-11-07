0c52b0efa6ac06009339aefe7d110ccf5c39f7ea
Rian <rian@advanced.net.tw>
2012-11-07 13:21:53 +0800

    created skeleton plus default config object

---
+++ index.js
@@ -0,0 +1,10 @@
+// usage 
+// var config = {
+//   "credential" : {
+//     "apiKey" : "key",
+//	   "cid" :"cid"
+//   }
+// }
+
+// var ean = require("ean")(config);
+module.exports = require('./lib/ean');
