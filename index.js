b596d606e0cffe69a9c7f96e6fdc37a9d4f61a74
Rian <rian@advanced.net.tw>
2012-11-07 17:18:45 +0800

    request room availability done(alpha)

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-// usage 
-// var config = {
-//   "credential" : {
-//     "apiKey" : "key",
-//	   "cid" :"cid"
-//   }
-// }
 
-// var ean = require("ean")(config);
 module.exports = require('./lib/ean');
